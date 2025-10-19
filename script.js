4d2f828c73b828e3e0a74f12809e1936a9f1f4ef
Ankith34 <ankithkumar64746@gmail.com>
2025-10-19 21:57:11 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+document.getElementById('billForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  const name = document.getElementById('name').value;
+  const amount = document.getElementById('amount').value;
+  const date = document.getElementById('date').value;
+
+  document.getElementById('b_name').innerText = name;
+  document.getElementById('b_amount').innerText = amount;
+  document.getElementById('b_date').innerText = date;
+
+  document.getElementById('bill').classList.remove('hidden');
+});
+
+document.getElementById('downloadBtn').addEventListener('click', function() {
+  const element = document.getElementById('bill');
+  html2canvas(element).then(canvas => {
+    const link = document.createElement('a');
+    link.download = 'bill.png';
+    link.href = canvas.toDataURL();
+    link.click();
+  });
+});
